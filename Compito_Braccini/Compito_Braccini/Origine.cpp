ed95b28baa9320342be8d6c40ad968435b2ca01b
malatesta14 <malatestaf37@gmail.com>
2018-12-10 09:07:51 +0100

    commit 1

---
+++ Compito_Braccini/Compito_Braccini/Origine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int contatore = 0;
+	for (int i = 1; i < 11; i++) {
+		contatore = i;
+		cout << contatore << endl;
+	}
+	system("pause");
+}
