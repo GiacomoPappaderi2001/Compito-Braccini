5883f55b5bac428393e03fa1118e3a4305160560
malatesta14 <malatestaf37@gmail.com>
2018-12-10 09:12:42 +0100

    commit 2

--- Compito_Braccini/Compito_Braccini/Origine.cpp
+++ Compito_Braccini/Compito_Braccini/Origine.cpp
@@ -7,5 +7,7 @@ int main() {
 		contatore = i;
 		cout << contatore << endl;
 	}
+	//linea che viene modificata per creare un conflitto
+	cout << "voglio creare un conflitto" << endl;
 	system("pause");
 }
